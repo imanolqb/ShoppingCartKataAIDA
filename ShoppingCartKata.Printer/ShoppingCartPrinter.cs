6e7238c945cd739bb2499a25a2e4f625bd7d0f32
Imanol Benito Quintero Bermúdez <imquintero@aidacanarias.com>
2024-10-30 20:56:59 +0000

    feat: ShoppingCartPrinter class

---
+++ ShoppingCartKata.Printer/ShoppingCartPrinter.cs
@@ -0,0 +1,25 @@
+﻿using ShoppingCartKata.Business;
+
+namespace ShoppingCartKata.Printer
+{
+    public class ShoppingCartPrinter
+    {
+        public void PrintShoppingCart(ShoppingCart cart)
+        {
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("| Producto     | Precio con IVA | Cantidad |");
+            Console.WriteLine("| -----------  | -------------- | -------- |");
+
+            foreach (var item in cart.Items)
+            {
+                var product = item.Key;
+                var amount = item.Value;
+                Console.WriteLine($"| {product.Name}   | {product.CalculateFinalPrice():0.00} €         | {amount}        |");
+                Console.WriteLine("|------------------------------------------|");
+                Console.WriteLine($"| Total productos: {cart.TotalAmountOfProducts()}                       |");
+                Console.WriteLine($"| Precio total: {cart.TotalPrice():0.00} €                     |");
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+    }
+}
