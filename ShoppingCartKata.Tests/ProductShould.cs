3ea23c49d6025ff212990a1c473f72894cbfd1bd
Imanol Benito Quintero Bermúdez <imquintero@aidacanarias.com>
2024-10-30 13:08:10 +0000

    refactor: product tests passed to ProductShould.cs

--- ShoppingCartKata.Tests/ProductShould.cs
+++ ShoppingCartKata.Tests/ProductShould.cs
@@ -6,6 +6,38 @@ namespace ShoppingCartKata.Tests
 {
     public class ProductShould
     {
+        private Product product1;
+        private Product product2;
+        private Product product3;
+
+        [SetUp]
+        public void Setup()
+        {
+            product1 = new Product("Lettuce", 1.55, 0.15, 0.21);
+            product2 = new Product("Tomato", 0.52, 0.15, 0.21);
+            product3 = new Product("Chicken", 1.34, 0.12, 0.21);
+        }
+
+        [Test]
+        public void Return_unit_price_of_product_given_product()
+        {
+            product1.CalculateUnitPrice().Should().Be(1.79);
+            product2.CalculateUnitPrice().Should().Be(0.60);
+            product3.CalculateUnitPrice().Should().Be(1.51);
+        }
+
+        /*
+        [Test]
+        public void Return_total_amount_when_a_lettuce_is_passed()
+        {
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddItem(new Product("Lettuce", 1.55));
+
+            var result = shoppingCart.TotalPrice();
+
+            result.Should().Be(2.17);
+        }
+        */
 
     }
 }
--- ShoppingCartKata.Tests/ShoppingCartShould.cs
+++ ShoppingCartKata.Tests/ShoppingCartShould.cs
@@ -8,17 +8,11 @@ namespace ShoppingCartKata.Tests
     public class ShoppingCartShould
     {
         private ShoppingCart shoppingCart;
-        private Product product1;
-        private Product product2;
-        private Product product3;
 
         [SetUp]
         public void Setup()
         {
             shoppingCart = new ShoppingCart();
-            product1 = new Product("Lettuce", 1.55, 0.15, 0.21);
-            product2 = new Product("Tomato", 0.52, 0.15, 0.21);
-            product3 = new Product("Chicken", 1.34, 0.12, 0.21);
         }
 
         [TestCase("Lettuce", 1.55)]
@@ -44,26 +38,5 @@ namespace ShoppingCartKata.Tests
 
             result.Should().Be(3);
         }
-
-        [Test]
-        public void Return_unit_price_of_product_given_product()
-        {
-            product1.CalculateUnitPrice().Should().Be(1.79);
-            product2.CalculateUnitPrice().Should().Be(0.60);
-            product3.CalculateUnitPrice().Should().Be(1.51);
-        }
-
-        /*
-        [Test]
-        public void Return_total_amount_when_a_lettuce_is_passed()
-        {
-            var shoppingCart = new ShoppingCart();
-            shoppingCart.AddItem(new Product("Lettuce", 1.55));
-
-            var result = shoppingCart.TotalPrice();
-
-            result.Should().Be(2.17);
-        }
-        */
     }
 }
